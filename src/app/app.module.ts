816953ef8be3ebf8c61c69ed01a2e863c9bee8f1
Enrique Villalón <EVillalonfcb@outlook.com>
2022-10-08 14:05:09 -0500

    Error de modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HistoryPageComponent } from './modules/history/pages/history-page/history-page.component';
 
 @NgModule({
   declarations: [ //TODO: Declaraciones, componentes, directivas, pipes  
-    AppComponent,
+    AppComponent, HistoryPageComponent,
 
   ],
   imports: [ //TODO: Solo se importan otros
