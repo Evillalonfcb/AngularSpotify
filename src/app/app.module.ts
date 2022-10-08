d73012d60151854001232e5386cac21e0c24102f
Enrique Villalón <EVillalonfcb@outlook.com>
2022-10-08 14:23:33 -0500

    Se arregla issue de history

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HistoryPageComponent } from './modules/history/pages/history-page/history-page.component';
+
 
 @NgModule({
   declarations: [ //TODO: Declaraciones, componentes, directivas, pipes  
-    AppComponent, HistoryPageComponent,
+    AppComponent
 
   ],
   imports: [ //TODO: Solo se importan otros
